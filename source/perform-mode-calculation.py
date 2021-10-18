3554c6bf90c99436e51149247ffd50e5a28b2f05
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-10-18 10:55:29 -0400

    feat: Add the perform-mode-calculation with all of the TODO markers.

---
+++ source/perform-mode-calculation.py
@@ -0,0 +1,21 @@
+# TODO: provide a docstring that summarizes all of the source code in this module
+
+# TODO: provide all of the required imports
+
+
+# TODO: define a function called calculate_mode that takes as input:
+#       a List[int]
+#       and produces as output:
+#       a Tuple[int, int]
+#
+# TODO: calculate the mode of the values in a list of numbers
+# return an ordered pair (i.e., a tuple) containing:
+# (the number that is the mode, the number of times the mode appeared)
+
+# TODO: Make sure that you understand why this conditional logic exists!
+if __name__ == "__main__":
+    # TODO: create a list of scores containing the values:
+    # 7, 8, 9, 2, 10, 9, 9, 9
+    # TODO: call the calculate_mode function with the list of scores
+    # TODO: display both the mode of the list and the frequency with which it was found
+    # TODO: look in Discord to see the expected output for this function
