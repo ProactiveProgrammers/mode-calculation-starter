4b1c2a3334cd6e345762b7f15ec2a6945b34cc9e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-04-07 07:25:17 -0400

    coms: Improve the comments and TODO markers inside of the perform-mode-calculation.py.

--- source/perform-mode-calculation.py
+++ source/perform-mode-calculation.py
@@ -1,5 +1,8 @@
 # TODO: provide a docstring that summarizes all of the source code in this module
 
+# TODO: Refer to the course textbook called "Doing Math with Python" to learn
+# more about how to calculate the mode of a list of values
+
 # TODO: provide all of the required imports
 
 
@@ -14,8 +17,8 @@
 
 # TODO: Make sure that you understand why this conditional logic exists!
 if __name__ == "__main__":
-    # TODO: create a list of scores containing the values:
+    # TODO: create a list of scores containing these values:
     # 7, 8, 9, 2, 10, 9, 9, 9
     # TODO: call the calculate_mode function with the list of scores
     # TODO: display both the mode of the list and the frequency with which it was found
-    # TODO: look in Discord to see the expected output for this function
+    # TODO: look on the course web site to see the expected output for this function
